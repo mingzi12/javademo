5ed4c34c948454545d361934d7dbae465b1fcda6
mingzi12 <690632338@qq.com>
2019-06-01 23:12:06 +0800

    java字符串和生成随机字符串demo。

---
+++ src/main/java/com/java/demo/RandomDemo.java
@@ -0,0 +1,41 @@
+package com.java.demo;
+
+import org.apache.commons.codec.binary.Base64;
+
+import java.io.UnsupportedEncodingException;
+import java.security.SecureRandom;
+
+/**
+ * @author TODO
+ * @description: TODO
+ * @date 2019/5/28 19:58
+ */
+public class RandomDemo {
+
+    private static final Base64 BASE_64_ENCONDER = new Base64();
+
+    public static String getRandomCode()
+    {
+        SecureRandom random = new SecureRandom();
+        byte[] bytes = new byte[32];
+        random.nextBytes(bytes);
+        String rCode = null;
+        try
+        {
+            rCode = new String(BASE_64_ENCONDER.encode(bytes), "UTF-8");
+        }
+        catch (UnsupportedEncodingException localUnsupportedEncodingException)
+        {
+            return "";
+        }
+        return rCode;
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 20; i++) {
+            String rCode = getRandomCode();
+            System.out.println(rCode);
+        }
+
+    }
+}
---
+++ src/main/java/com/java/demo/TestString.java
@@ -0,0 +1,11 @@
+package com.java.demo;
+
+public class TestString {
+
+    public static void main(String[] args) {
+        System.out.println("xxxxxxxx");
+
+        String[] players = new String[]{"Kobe", "Mike"};
+        System.out.println(players);
+    }
+}
