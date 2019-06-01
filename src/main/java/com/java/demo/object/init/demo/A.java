61f74c151492eee70a9379954be49fb051469536
mingzi12 <690632338@qq.com>
2019-06-01 23:09:04 +0800

    java对象初始化顺序demo。

---
+++ src/main/java/com/java/demo/object/init/demo/A.java
@@ -0,0 +1,21 @@
+package com.java.demo.object.init.demo;
+
+/**
+ * @author TODO
+ * @description: TODO
+ * @date 2019/5/27 17:32
+ */
+public class A {
+
+    static{
+
+        System.out.print("A");
+
+    }
+
+    A(){
+
+        System.out.print("a");
+
+    }
+}
