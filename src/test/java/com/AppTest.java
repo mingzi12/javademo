06bef651cd5c125efa0dcda1d59bedd6306935e7
mingzi12 <690632338@qq.com>
2022-10-30 10:36:07 +0800

    测试InetAddress接口获取本地机器信息

--- src/test/java/com/AppTest.java
+++ src/test/java/com/AppTest.java
@@ -4,10 +4,13 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class AppTest
 {
     /**
      * Rigorous Test :-)
@@ -16,5 +19,18 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void getIP(){
+        try {
+            InetAddress address = InetAddress.getLocalHost();
+            String ip = address.getHostAddress().toString();//获取本机IP
+            String hostName = address.getHostName().toString();//获取本机计算机名称
+            System.out.println("ip is:"+ip);
+            System.out.println("host name is:"+hostName);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
     }
 }
